0dd1f1748edf976a4621d977d7b3f1c87f41d1a8
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-01-23 21:04:55 +0600

    addes js

---
+++ script.js
@@ -0,0 +1,29 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*='${id}']`).classList.add('active');
+            });
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    if (menuIcon.classList.contains('ri-menu-line')) {
+        menuIcon.classList.replace('ri-menu-line', 'ri-close-line');
+    } else {
+        menuIcon.classList.replace('ri-close-line', 'ri-menu-line');
+    }
+    navbar.classList.toggle('active');
+};
